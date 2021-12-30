92feaefb736096882a70b07efd2052a36f42669f
Daniel Samuel <dansam5000@gmail.com>
2021-12-30 09:44:31 +0100

    Add school paper work solutions

--- 8kyu-solutions/solution.js
+++ 8kyu-solutions/solution.js
@@ -1,2 +1,10 @@
-// 1. multiply
+// Test multiply
 const multiply = (a, b) => a * b;
+
+// 1. School paper work
+const paperwork = (n, m) => {
+  if (n < 0 || m < 0) {
+    return 0;
+  }
+  return n * m;
+};

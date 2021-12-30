ed2a621032b42394b98444e892a22e5e3699b642
Daniel Samuel <dansam5000@gmail.com>
2021-12-30 09:50:15 +0100

    Add calc bmi solution

--- 8kyu-solutions/solution.js
+++ 8kyu-solutions/solution.js
@@ -8,3 +8,17 @@ const paperwork = (n, m) => {
   }
   return n * m;
 };
+
+// 2. Calculate Body Mass Index
+const bmi = (weight, height) => {
+  let result = weight / Math.pow(height, 2);
+  if (result <= 18.5) {
+    return 'Underweight';
+  } else if (result <= 25.0) {
+    return 'Normal';
+  } else if (result <= 30.0) {
+    return 'Overweight';
+  } else {
+    return 'Obese';
+  }
+};

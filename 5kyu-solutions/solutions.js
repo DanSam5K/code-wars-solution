c460abe06054df94407bbeb5cfc73c6e60b7da7a
Daniel Samuel <dansam5000@gmail.com>
2022-02-19 11:28:55 +0100

    Add word anagram solution

---
+++ 5kyu-solutions/solutions.js
@@ -0,0 +1,14 @@
+// anagrams functions
+function anagrams(word, words) {
+  let result = [];
+  for (let element of words) {
+    if (element.length === word.length) {
+      let newElement = element.split('').sort().join('');
+      let newWord = word.split('').sort().join('');
+      if (newElement === newWord) {
+        result.push(element);
+      }
+    }
+  }
+  return result;
+}

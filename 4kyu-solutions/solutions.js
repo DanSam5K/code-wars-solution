aa4b30bdf4e196e027f4cdb03334dd6ba78b999b
Daniel Samuel <dansam5000@gmail.com>
2022-03-14 21:47:51 +0100

    Add 4kyu challenge solution

---
+++ 4kyu-solutions/solutions.js
@@ -0,0 +1,18 @@
+// 1. Observed pin solution
+
+function getPINs(observed) {
+  // TODO: This is your job, detective!
+  const combination = [];
+  const neighbors = {
+    0: ['8'],
+    1: ['2', '4'],
+    2: ['1', '3', '5'],
+    3: ['2', '6'],
+    4: ['1', '5', '7'],
+    5: ['2', '4', '6', '8'],
+    6: ['3', '5', '9'],
+    7: ['4', '8'],
+    8: ['5', '7', '9', '0'],
+    9: ['6', '8'],
+  };
+}

441b81396bd837dbf67c744325b4aef7ea916a3b
Daniel Samuel <dansam5000@gmail.com>
2022-04-22 14:00:01 +0100

    Add changes and new solution

--- 8kyu-solutions/solution.js
+++ 8kyu-solutions/solution.js
@@ -143,8 +143,11 @@ function fakeBin2(x) {
   return x.replace(/[0-4]/g, '0').replace(/[5-9]/g, '1');
 }
 
-// 19.
+// 19. fake
 
 function fakeBin2(x) {
   return x.replace(/[0-4]/g, '0').replace(/[5-9]/g, '1');
 }
+
+// 20 . Fake Binary
+

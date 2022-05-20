91c87a1e50c0b202f5e46267dfd90567231fb279
Daniel Samuel <dansam5000@gmail.com>
2022-05-20 08:17:10 +0100

    what is between rb soln

--- 8kyu-solutions/solution.rb
+++ 8kyu-solutions/solution.rb
@@ -3,13 +3,18 @@ def get_volume_of_cuboid(length, width, height)
   length * width * height
 end
 
-#2. Convert String to Integer
+# 2. Convert String to Integer
 def string_to_integer(string)
   string.to_i
 end
 
-#3. return negative number
+# 3. return negative number
 def negative(number)
   number unless number > 0
   -number
+end
+
+# 4. What is between
+def between(a, b)
+  (a..b).to_a
 end

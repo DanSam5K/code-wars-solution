cb0dcbadf8a7298958289e2ff7d22421c95fbe24
Daniel Samuel <dansam5000@gmail.com>
2022-05-23 07:06:33 +0100

    add solution mim sum of array js

--- 7kyu-solutions/solutions.js
+++ 7kyu-solutions/solutions.js
@@ -123,3 +123,17 @@ function dontGiveMeFive(start, end)
   }
   return count;
 }
+
+// 12. Min sum of array
+function minSum(arr){
+  arr = arr.sort(function (a, b) {return a - b});
+  let i = 0;
+  let j = arr.length - 1;
+  let sum = 0;
+  while (i < j ) {
+    sum += arr[i] * arr[j];
+    i++;
+    j--;
+  }
+  return sum;
+}

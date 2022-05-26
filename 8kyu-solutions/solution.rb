cf1c55fbdda0cded8792e3388d79a50f9a56ceff
Daniel Samuel <dansam5000@gmail.com>
2022-05-26 21:28:51 +0100

    Add 8kyu code wars solution on #howMuchILoveYOu rb

--- 8kyu-solutions/solution.rb
+++ 8kyu-solutions/solution.rb
@@ -24,3 +24,17 @@ def sum_of_differences(array)
   return 0 unless array.size > 1
   array.sort.each_cons(2).map { |a, b| b - a }.reduce(:+)
 end
+
+# 6. I love you, a little , a lot, passionately ... not at all
+def how_much_i_love_you(nb_petals)
+  case nb_petals
+  when 0..7
+    'I love you'
+  when 8..15
+    'a little'
+  when 16..100
+    'a lot'
+  else
+    'passionately'
+  end
+end

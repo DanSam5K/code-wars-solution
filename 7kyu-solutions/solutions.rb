a34016ab2e7581225e75e9e78377e43f2913a502
Daniel Samuel <dansam5000@gmail.com>
2022-05-28 09:26:23 +0100

    Narcissistic number solution in rb

--- 7kyu-solutions/solutions.rb
+++ 7kyu-solutions/solutions.rb
@@ -11,3 +11,13 @@ def min_sum(arr)
     end
     sum
 end
+
+# 2. Narcissistic Numbers
+def is_narcissistic(num)
+  num_arr = num.to_s.split('')
+  sum = 0
+  num_arr.each do |n|
+    sum += n.to_i ** num_arr.size
+  end
+  sum == num
+end

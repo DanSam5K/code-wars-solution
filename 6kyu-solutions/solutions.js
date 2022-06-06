c2885ea7f3dbbf4e96a176b59df7f6d617e453e4
Daniel Samuel <dansam5000@gmail.com>
2022-06-06 21:55:43 +0100

    add second solution mad-max recursion js

--- 6kyu-solutions/solutions.js
+++ 6kyu-solutions/solutions.js
@@ -109,3 +109,21 @@ function max(array) {
 
 //. 10b. Mad Max: Recursion road solution
 
+function max(array) {
+  if(array.length === 0 ){
+    return -Infinity;
+  }
+  else if(array.length === 1){
+    return array[0];
+  }
+  else if(array.length >= 2){
+    if(array[0] <= array[1]){
+      array.splice(0,1);
+      return max(array);
+    }
+    else{
+      array.splice(1,1);
+      return max(array);
+    }
+  }
+}

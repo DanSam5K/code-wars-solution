25a3ec6807cecf472b5efe16c1f15698a212954c
Daniel Samuel <dansam5000@gmail.com>
2022-06-09 23:12:54 +0100

    Add queue_time solution in rb

--- 6kyu-solutions/solutions.rb
+++ 6kyu-solutions/solutions.rb
@@ -1,5 +1,14 @@
 # 1. Mad Max: Recursion Road
 def max(arr)
-    return arr[0] if arr.length == 1
-    arr[0] > arr[1] ? arr[0] : max(arr[1..-1])
+  return arr[0] if arr.length == 1
+  arr[0] > arr[1] ? arr[0] : max(arr[1..-1])
+end
+
+# 2. The Supermarket Queue
+def queue_time(customers, n)
+  queue = Array.new(n, 0)
+  customers.each { |customer| 
+    queue[queue.index(queue.min)] += customer
+  }
+  queue.max
 end

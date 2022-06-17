540da629b5c2f9e9733d0de21b2214e7b34e35a1
Daniel Samuel <dansam5000@gmail.com>
2022-06-17 17:44:42 +0100

    number of pairs solution

--- 6kyu-solutions/solutions.rb
+++ 6kyu-solutions/solutions.rb
@@ -12,3 +12,23 @@ def queue_time(customers, n)
   }
   queue.max
 end
+
+# 3. Pairs of gloves
+def number_of_pairs(n)
+  return 0 if n == 0
+  return 1 if n == 1
+  return number_of_pairs(n - 1) + number_of_pairs(n - 2)
+end
+
+
+# function numberOfPairs(gloves){
+#   let pairs = 0;
+#   for(let i=0; i<gloves.length; i++){
+#     for(let j=i+1; j<gloves.length; j++){
+#       if(gloves[i] === gloves[j]){
+#         pairs++;
+#       }
+#     }
+#   }
+#   return pairs;
+# }

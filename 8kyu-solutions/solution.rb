6880f9aba7aeffc4365f7252decac47e1e997b3f
Daniel Samuel <dansam5000@gmail.com>
2022-12-08 10:00:10 +0100

    Solve #bar_traing 8ky challenge

--- 8kyu-solutions/solution.rb
+++ 8kyu-solutions/solution.rb
@@ -68,3 +68,8 @@ end
 def positive_sum(arr)
   arr.select { |number| number > 0 }.reduce(:+)
 end
+
+# 13. Localize The Barycenter of a Triangle solution
+def bar_triang(p1, p2, p3)
+  [((p1[0] + p2[0] + p3[0]) / 3.0).round(4), ((p1[1] + p2[1] + p3[1]) / 3.0).round(4)]
+end

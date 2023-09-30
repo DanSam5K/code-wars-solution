7fa532d9c4c80a9ee7d3723088303acb6866f77c
Daniel Samuel <dansam5000@gmail.com>
2023-09-30 16:46:49 +0200

    find missing letter sln

--- 4kyu-solutions/solutions.js
+++ 4kyu-solutions/solutions.js
@@ -61,3 +61,14 @@ function exquisiteCorpse(part1, part2) {
   }
   return result;
 }
+
+// 5. Find the missing letter solution
+function findMissingLetter(array) {
+  let result = '';
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].charCodeAt() + 1 !== array[i + 1].charCodeAt()) {
+      result = String.fromCharCode(array[i].charCodeAt() + 1);
+    }
+  }
+  return result;
+}

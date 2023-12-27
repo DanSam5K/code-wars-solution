36567a9defccc7eb8d82b913d9dfbe75588e6f11
Daniel Samuel <dansam5000@gmail.com>
2023-12-27 20:52:58 +0100

    Update solution

--- algo-problems/common-chal.js
+++ algo-problems/common-chal.js
@@ -74,3 +74,14 @@ function fibonacci(num) {
 }
 
 console.log(fibonacci(9));
+
+// 2nd solution
+function fibonacci2(num) {
+  const result = [0, 1];
+  for (let i = 2; i <= num; i++) {
+    const prevNum1 = result[i - 1];
+    const prevNum2 = result[i - 2];
+    result.push(prevNum1 + prevNum2);
+  }
+  return result[num];
+}

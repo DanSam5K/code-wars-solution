5b262b5edb5a4bf020b818110827aa0dc68b84f7
Daniel Samuel <dansam5000@gmail.com>
2024-02-21 23:24:18 +0100

    Sum oposiitibe sum

--- 8kyu-solutions/solution.js
+++ 8kyu-solutions/solution.js
@@ -250,3 +250,15 @@ function firstNonConsecutive(arr) {
   }
   return null;
 }
+
+// 30. Sum of positive
+
+function positiveSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
